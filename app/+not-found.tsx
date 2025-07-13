c1b35ce2e146e0360531d12cc6bbfd3213c9af50
Albert R. González‑Marín <dsifriend@gmail.com>
2025-07-13 04:18:06 +0200

    Customized 404 page with flavor text

--- app/+not-found.tsx
+++ app/+not-found.tsx
@@ -1,19 +1,23 @@
 import { Link, Stack } from 'expo-router';
-import { StyleSheet } from 'react-native';
-
-import { ThemedText } from '@/components/ThemedText';
-import { ThemedView } from '@/components/ThemedView';
+import { StyleSheet, Text, useColorScheme, View } from 'react-native';
 
 export default function NotFoundScreen() {
+  const textColor = useColorScheme() === 'dark' ? '#fff' : '#000';
+  const linkColor = useColorScheme() === 'dark' ? '#1e90ff' : '#007bff';
+
   return (
     <>
-      <Stack.Screen options={{ title: 'Oops!' }} />
-      <ThemedView style={styles.container}>
-        <ThemedText type="title">This screen does not exist.</ThemedText>
-        <Link href="/" style={styles.link}>
-          <ThemedText type="link">Go to home screen!</ThemedText>
+      <Stack.Screen options={{ title: 'Wibbly Wobbly, Timey-Wimey Stuff' }} />
+      <View style={styles.container}>
+        <Text style={[styles.flavorText, { color: textColor }]}>
+          People assume that time is a strict progression of cause to effect,
+          but actually from a non-linear, non-subjective viewpoint,
+          it's more like a big ball of wibbly wobbly, timey-wimey stuff
+        </Text>
+        <Link href="/" style={[styles.link, { color: linkColor }]}>
+          There might be a way out. A delivery hatch or something.
         </Link>
-      </ThemedView>
+      </View>
     </>
   );
 }
@@ -24,6 +28,11 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
     padding: 20,
+  },
+  flavorText: {
+    fontSize: 16,
+    textAlign: 'center',
+    marginBottom: 20,
   },
   link: {
     marginTop: 15,
